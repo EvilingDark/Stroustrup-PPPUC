5af67ebf172e6849927b712cd86204a30943940a
EvilingDark <evilingdark@mail.ru>
2020-01-15 21:44:41 +1000

    add chapter 8

---
+++ chapter08/01/Header.h
@@ -0,0 +1,67 @@
+#pragma once
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include <forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <array>
+#include <regex>
+#include<random>
+#include<stdexcept>
+
+using namespace std;
+
+//программа со скобками и переменными
+inline void error(const string& s)
+{
+	throw runtime_error(s);
+}
+
+inline void error(const string& s, const string& s2)
+{
+	error(s + s2);
+}
+
+inline void error(const string& s, int i)
+{
+	ostringstream os;
+	os << s << ": " << i;
+	error(os.str());
+}
+
+template<class R, class A> R narrow_cast(const A& a)
+{
+	R r = R(a);
+	if (A(r) != a) error(string("info loss"));
+	return r;
+}
+
+inline void keep_window_open()
+{
+	cin.clear();
+	cout << "Please enter a character to exit\n";
+	char ch;
+	cin >> ch;
+	return;
+}
+
+inline void keep_window_open(string s)
+{
+	if (s == "") return;
+	cin.clear();
+	cin.ignore(120, '\n');
+	for (;;) {
+		cout << "Please enter " << s << " to exit\n";
+		string ss;
+		while (cin >> ss && ss != s)
+			cout << "Please enter " << s << " to exit\n";
+		return;
+	}
+}
---
+++ chapter08/drill/my.h
@@ -0,0 +1,5 @@
+extern int foo;
+void print_foo();
+void print(int i);
+void swap_v(int a, int b);
+void swap_r(int& a, int& b);
